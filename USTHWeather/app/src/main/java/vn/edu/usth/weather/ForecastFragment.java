99e3adbd65d9954eac9ebe7cbfca5a8d60222c8b
Kahn <vietdung1607@gmail.com>
2019-11-23 12:13:03 +0700

    Labwork #5 commit
    Change the fragment into dynamic type,
    fix the background

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -11,18 +11,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ForecastFragment extends Fragment {
-    public ForecastFragment() {
-
-    }
-
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        super.onCreateView(inflater,container,savedInstanceState);
-        View v = inflater.inflate(R.layout.fragment_forecast, container,false);
-        v.setBackgroundColor(0xFF160799);
+
+        final String days[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        ViewGroup v = (ViewGroup) inflater.inflate(R.layout.fragment_forecast,null,false);
+        LinearLayout linearLayout = v.findViewById(R.id.forecast_fragment);
+        for(int i=0; i<9; ++i){
+            View row = inflater.inflate(R.layout.weather_row,container,false);
+            ((TextView)row.findViewById(R.id.day)).setText(days[i%7]);
+            ((ImageView)row.findViewById(R.id.weather)).setImageResource(R.drawable.cloudy);
+            linearLayout.addView(row);
+        }
         return v;
     }
 }
