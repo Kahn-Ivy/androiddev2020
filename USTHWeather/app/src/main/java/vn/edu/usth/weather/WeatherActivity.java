8304a63741bc79ef25f8ee96053bf9aa69c559d9
Khan <kl14n.Js@gmail.com>
2019-12-16 23:17:03 +0700

    Labwork 12

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -11,9 +11,15 @@ import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -29,6 +35,18 @@ import vn.edu.usth.weather.HomeFragmentPagerAdapter;
 public class WeatherActivity extends AppCompatActivity {
     MediaPlayer mp;
     private final static String TAG = "TestActivity";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,24 +58,24 @@ public class WeatherActivity extends AppCompatActivity {
         pager.setAdapter(adapter);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
+
         //writeExternal();
 
-        new Thread(new Runnable() {
+        /*new Thread(new Runnable() {
             @Override
             public void run() {
-                mp = MediaPlayer.create(getApplicationContext(), R.raw.marimba);
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.dauchiungoiyen);
                 mp.start();
             }
-        }).start();
+        }).start();*/
 
         Log.i(TAG, "On Create .....");
     }
-    private void writeExternal() {
-        String filename = "marimba.mp3";
+    /*private void writeExternal() {
+        String filename = "dauchiungoiyen.mp3";
         String filepath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data/";
-
         try {
-            InputStream is = getApplicationContext().getResources().openRawResource(R.raw.marimba);
+            InputStream is = getApplicationContext().getResources().openRawResource(R.raw.dauchiungoiyen);
             File myFile = new File(filepath + filename);
             if(!myFile.exists()){
                 try{
@@ -76,8 +94,7 @@ public class WeatherActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-    }
+    }*/
     @Override
     protected void onDestroy() {
         super.onDestroy();
